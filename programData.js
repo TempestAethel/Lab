9a547ebe08e587e673aad8cf81d73dd8b7d173ae
RoyalRealm <Lightseeksdark@gmail.com>
2024-11-14 04:28:31 +0530

    Update programData.js

--- programData.js
+++ programData.js
@@ -1,3 +1,59 @@
+`
+
+const programData = {
+    subjects: {
+        DSP: [
+            {
+                header: "DSP Program Placeholder",
+                question: "DSP Program Question Placeholder",
+                code: "% DSP Program Code Placeholder"
+            },
+            // Additional DSP programs can be added here
+            /*
+            {
+                header: "Additional DSP Program Placeholder",
+                question: "Additional DSP Program Question Placeholder",
+                code: "% Additional DSP Program Code Placeholder"
+            }
+            */
+        ],
+        DC: [
+            {
+                header: "DC Program Placeholder",
+                question: "DC Program Question Placeholder",
+                code: "% DC Program Code Placeholder"
+            },
+            // Additional DC programs can be added here
+            /*
+            {
+                header: "Additional DC Program Placeholder",
+                question: "Additional DC Program Question Placeholder",
+                code: "% Additional DC Program Code Placeholder"
+            }
+            */
+        ],
+        
+        // Future placeholders for another subject set (commented out)
+        /*
+        AnotherSubject: [
+            {
+                header: "Another Subject Program Placeholder",
+                question: "Another Subject Program Question Placeholder",
+                code: "% Another Subject Program Code Placeholder"
+            },
+            // Additional programs for AnotherSubject can be added here
+            {
+                header: "Additional AnotherSubject Program Placeholder",
+                question: "Additional AnotherSubject Program Question Placeholder",
+                code: "% Additional AnotherSubject Program Code Placeholder"
+            }
+        ]
+        */
+    }
+}
+
+`
+    
 const programData = {
     subjects: {
         DSP: [
